3f8ded081e7bc3c4d246a3b683651ddd40a41243
Gregory Getchell <gregorygetchell@Gregorys-MacBook-Air.local>
2020-08-11 12:45:28 -0400

    Added Devise and Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: true
+  validates :first_name, presence: true 
+  validates :last_name, presence: true
+
+  has_many :posts   
+  has_many :comments
 end
